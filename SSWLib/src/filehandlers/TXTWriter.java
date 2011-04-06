d1006d6ddda7bd656c5a2551624978fb200fa2a9
gblouin <gblouin@cffa588f-95e4-4d92-9590-e58d669f6d1b>
2011-04-06 17:25:19 +0000

    Added ability to export current Mech list to csv

--- SSWLib/src/filehandlers/TXTWriter.java
+++ SSWLib/src/filehandlers/TXTWriter.java
@@ -28,7 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package filehandlers;
 
-import Force.Force;
+import Force.*;
 import battleforce.BFConstants;
 import common.*;
 import java.io.BufferedWriter;
@@ -39,6 +39,10 @@ import java.util.Vector;
 import battleforce.BattleForceStats;
 import battleforce.BattleForceTools;
 import components.*;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import list.*;
+import utilities.CostBVBreakdown;
 
 public class TXTWriter {
 
@@ -1193,5 +1197,173 @@ public class TXTWriter {
             }
         }
         return retval;
+    }
+
+
+    public void WriteList(String filename, Scenario scenario) throws IOException {
+        if ( !filename.endsWith(".csv") ) { filename += ".csv"; }
+        BufferedWriter FR = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( filename ), "UTF-8" ) );
+
+        FR.write( CSVFormat("unit_type") );
+        FR.write( CSVFormat("sub_unit_type") );
+        FR.write( CSVFormat("unit_name") );
+        FR.write( CSVFormat("model_number") );
+        FR.write( CSVFormat("tonnage") );
+        FR.write( CSVFormat("bv2") );
+        FR.write( CSVFormat("tw rules_level") );
+        FR.write( CSVFormat("technology base") );
+        FR.write( CSVFormat("source") );
+        FR.write( CSVFormat("date") );
+        FR.write( CSVFormat("era") );
+        FR.write( "PV,Wt,MV,S,M,L,E,OV,Armor,Internal,Special Abilities" );
+        FR.newLine();
+
+        String datum = "";
+        for ( Unit u : scenario.getUnits() ) {
+            FR.write( CSVFormat(CommonTools.UnitTypes[u.UnitType]) );
+            datum = "BattleMech";
+            if ( u.IsOmni() ) { datum = "OmniMech"; }
+            FR.write( CSVFormat(datum) );
+            FR.write( CSVFormat(u.Type ));
+            FR.write( CSVFormat((u.Model + " " + u.Configuration).trim()) );
+            FR.write( CSVFormat(u.Tonnage+"") );
+            FR.write( CSVFormat(u.BaseBV+"") );
+            FR.write( CSVFormat("") );
+            FR.write( CSVFormat("") );
+            FR.write( CSVFormat("") );
+            FR.write( CSVFormat("") );
+            FR.write( CSVFormat("") );
+            FR.write( u.getBFStats().SerializeCSV( false ) );
+            FR.newLine();
+        }
+        FR.close();
+    }
+
+    public void WriteList(String filename, MechList list) throws IOException {
+        if ( !filename.endsWith(".csv") ) { filename += ".csv"; }
+        BufferedWriter FR = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( filename ), "UTF-8" ) );
+
+        FR.write( CSVFormat("unit_type") );
+        FR.write( CSVFormat("sub_unit_type") );
+        FR.write( CSVFormat("unit_name") );
+        FR.write( CSVFormat("model_number") );
+        FR.write( CSVFormat("tonnage") );
+        FR.write( CSVFormat("bv2") );
+        FR.write( CSVFormat("tw rules_level") );
+        FR.write( CSVFormat("technology base") );
+        FR.write( CSVFormat("source") );
+        FR.write( CSVFormat("date") );
+        FR.write( CSVFormat("era") );
+        FR.write( "PV,Wt,MV,S,M,L,E,OV,Armor,Internal,Special Abilities" );
+        FR.newLine();
+
+        String datum = "";
+        for (int i=0; i < list.Size(); i++) {
+            MechListData data = (MechListData) list.Get(i);
+            FR.write( CSVFormat("BattleMech") );
+            datum = "BattleMech";
+            if ( data.isOmni() ) { datum = "OmniMech"; }
+            FR.write( CSVFormat(datum) );
+            FR.write( CSVFormat(data.getName()) );
+            FR.write( CSVFormat((data.getModel() + " " + data.getConfig()).trim()) );
+            FR.write( CSVFormat(data.getTonnage()+"") );
+            FR.write( CSVFormat(data.getBV()+"") );
+            FR.write( CSVFormat(data.getLevel()) );
+            FR.write( CSVFormat(data.getTech()) );
+            FR.write( CSVFormat(data.getSource()) );
+            FR.write( CSVFormat(data.getYear()+"") );
+            FR.write( CSVFormat(data.getEra()) );
+            FR.write( data.getBattleForceStats().SerializeCSV( false ) );
+            FR.newLine();
+        }
+        FR.close();
+    }
+
+    public void WriteCost( Mech m, String filename ) throws IOException {
+        filename += m.GetFullName();
+        if ( !filename.endsWith(".txt") ) { filename += ".txt"; }
+        BufferedWriter FR = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( filename ), "UTF-8" ) );
+
+        CostBVBreakdown cost = new CostBVBreakdown(m);
+        FR.write(cost.Render());
+        FR.close();
+    }
+
+    public void WriteBFList(String filename, MechList list) throws IOException {
+        if ( !filename.endsWith(".csv") ) { filename += ".csv"; }
+        BufferedWriter FR = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( filename ), "UTF-8" ) );
+
+        String message = "";
+        FR.write("Element,PV,Wt,MV,S,M,L,E,OV,Armor,Internal,Special Abilities");
+        FR.newLine();
+        for ( MechListData mech : list.getList() ) {
+            FR.write(mech.getBattleForceStats().SerializeCSV( true ));
+            FR.newLine();
+        }
+
+        FR.close();
+
+        if ( !message.isEmpty() ) {
+            Media.Messager("Could not write out the following:\n" + message);
+        }
+    }
+
+    public void WriteBFList(String filename, Scenario scenario) throws IOException {
+        if ( !filename.endsWith(".csv") ) { filename += ".csv"; }
+        BufferedWriter FR = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( filename ), "UTF-8" ) );
+
+        String message = "", force= "", group = "";
+        FR.write("Force,Group,Element,PV,Wt,MV,S,M,L,E,OV,Armor,Internal,Special Abilities");
+        FR.newLine();
+        for ( Force f : scenario.getForces() ) {
+            force = f.ForceName;
+            for ( Unit u : f.getUnits() ) {
+                group = u.getGroup();
+                FR.write(CSVFormat(force) + CSVFormat(group) + u.getBFStats().SerializeCSV( true ));
+                FR.newLine();
+            }
+        }
+
+        FR.close();
+
+        if ( !message.isEmpty() ) {
+            Media.Messager("Could not write out the following:\n" + message);
+        }
+    }
+
+    public void WriteFactorList(String filename, Scenario scenario ) throws IOException {
+        if ( !filename.endsWith(".csv") ) { filename += ".csv"; }
+        BufferedWriter FR = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( filename ), "UTF-8" ) );
+
+        String message = "";
+        FR.write("Unit,Type,Prb,ECM,Spd,Jmp,TSM,Phys,Armr,TC,8+,10+,Hd Cap,Tot Dmg,Base BV, Adj BV");
+        FR.newLine();
+        for ( Force f : scenario.getForces() ) {
+            if ( !f.ForceName.isEmpty()) {FR.write(f.ForceName);}
+            for ( Group g : f.Groups ) {
+                FR.newLine();
+                for ( Unit u : g.getUnits() ) {
+                    FR.write(u.SerializeFactors());
+                    FR.newLine();
+                }
+            }
+            FR.write(f.SerializeFactors());
+            FR.newLine();
+            FR.newLine();
+        }
+
+        FR.close();
+
+        if ( !message.isEmpty() ) {
+            Media.Messager("Could not write out the following:\n" + message);
+        }
+
+    }
+
+    public String CSVFormat( String data ) {
+        if ( data.contains(",") )
+            return "\"" + data + "\",";
+        else
+            return data + ",";
     }
 }
